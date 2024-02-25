cb797a180b845a47563032e2bd3bb16ce59a6449
hsuifang <whalekiller03@gmail.com>
2024-02-26 01:47:47 +0800

    feat: Add Schedule in home

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from "react";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "@/pages/Home/index.jsx";
 import Footer from "@/components/Footer/index.jsx";
 import Navigation from "@/components/Navigation/index.jsx";
 
 import "./App.css";
 
+const Layout = () => (
+  <div className="md:h-[100vh] md:overflow-y-hidden">
+    <Navigation />
+    <main className="container mx-auto py-4 md:py-10">
+      <Outlet />
+    </main>
+    <Footer />
+  </div>
+);
+
 function App() {
   return (
-    <>
-      <Navigation />
-      <Home />
-      <Footer />
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<div>asdf</div>} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const Navigation = () => {
-  return <div data-testid="navigation">Navigation</div>;
+  return (
+    <header
+      className="border-b shadow bg-white sticky top-0 z-10"
+      data-testid="navigation"
+    >
+      <div className="container mx-auto px-4 py-6 flex items-center ">
+        <Link to={"/"}>
+          <h1>Logo</h1>
+        </Link>
+
+        <nav className="contents mx-auto text-base lg:text-lg">
+          <ul className="mx-auto flex items-center">
+            <li className="px-5 lg:px-8 active">
+              <Link to={"/"}>行程表</Link>
+            </li>
+            <li className="px-5 lg:px-8">
+              <Link to={"/"}>注意事項</Link>
+            </li>
+            <li className="px-5 lg:px-8">
+              <Link to={"/"}>人員</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Navigation;
